d10f8ab370a84792104e145f267b4bbcaca78509
hitesh9811 <33113705+hitesh9811@users.noreply.github.com>
2020-10-02 01:25:59 +0530

    Update 12.php

--- 12.php
+++ 12.php
@@ -3,5 +3,5 @@ $target_days = mktime(0,0,0,3,8,2017);
 $today = time();
 $diff_days = ($target_days - $today);
 $days = (int)($diff_days/86400);
-print "Days till next birthday: $days days!"."\n";
+print "Days till next binod's birthday: $days days!"."\n";
 ?>
